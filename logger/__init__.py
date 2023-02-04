5125aca6960d3049b7e3be23b066f79c00239fac
Andrey Nikolaevich Korenyuk <korenyuk.a.n@mail.ru>
2023-02-04 15:11:33 +0500

    Version 0.1.08_DI
    Забыл добавить RequestRecordFromSQLyte в init

--- logger/__init__.py
+++ logger/__init__.py
@@ -19,6 +19,8 @@ from logger.obj.file_printer import StandardObjectFilePrinter
 from logger.obj.file_printer import StandardObjectFileSaver
     # Logger`ы
 from logger.obj.loggerer import Logger
+    # Работа с БД
+from logger.obj.request_record_from_sqlyte import RequestRecordFromSQLyte
     # Вывод в консоль
 from logger.obj.terminal_printer import StandardResultTerminalPrinter
 from logger.obj.terminal_printer import StandardObjectTerminalPrinter
