1d5426bc0f2e1399cb1f783ceb5fda2a7482e85e
Andrey Nikolaevich Korenyuk <korenyuk.a.n@mail.ru>
2023-02-04 14:59:43 +0500

    Version 0.1.07_DI
    Добавил все классы пакета в init

--- logger/__init__.py
+++ logger/__init__.py
@@ -5,11 +5,22 @@
 from logger.obj.constants import DECODING
 
 # Классы пакета
+    # Abstractclass
 from logger.obj.abstract_classes import Dictionarer
 from logger.obj.abstract_classes import Notifier
+from logger.obj.abstract_classes import RecordRequester
+    # Каталогеры
+from logger.obj.cataloger import Cataloger
+    # Пользовательские ошибки
+from logger.obj.exceptions import InvalidValue
+    # Вывод в файл
 from logger.obj.file_printer import StandardResultFilePrinter
 from logger.obj.file_printer import StandardObjectFilePrinter
 from logger.obj.file_printer import StandardObjectFileSaver
+    # Logger`ы
 from logger.obj.loggerer import Logger
+    # Вывод в консоль
 from logger.obj.terminal_printer import StandardResultTerminalPrinter
 from logger.obj.terminal_printer import StandardObjectTerminalPrinter
+    # контейнеры
+from logger.obj.containers import Container
