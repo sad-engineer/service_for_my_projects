541b7293b1e84ecfe876ff5db8f0a750d9ad13ef
Andrey Nikolaevich Korenyuk <korenyuk.a.n@mail.ru>
2023-07-15 11:16:01 +0500

    Version 0.1.34_DI
    Правки в связи с переводом проекта в онлайн

--- service/__init__.py
+++ service/__init__.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from .obj.abstract_classes import Dictionarer
-from .obj.abstract_classes import Notifier
-from .obj.abstract_classes import RecordRequester
-from .obj.cataloger import Cataloger
-from .obj.checker_in_dict import CheckerInDictionary
-from .obj.decorators import timeit
-from .obj.decorators import timeit_property
-from .obj.decorators import logged
-from .obj.decorators import output_debug_message_for_init_method
-from .obj.containers import Requester
-from .obj.exceptions import CreationError, InvalidValue, InvalidTypeValue, ReceivedEmptyDataFrame, InvalidModuleName
-from .obj.request_record_from_sqlyte import RequestRecordFromSQLyte
+from service.obj.abstract_classes import Dictionarer
+from service.obj.abstract_classes import Notifier
+from service.obj.abstract_classes import RecordRequester
+from service.obj.cataloger import Cataloger
+from service.obj.checker_in_dict import CheckerInDictionary
+from service.obj.decorators import timeit
+from service.obj.decorators import timeit_property
+from service.obj.decorators import logged
+from service.obj.decorators import output_debug_message_for_init_method
+from service.obj.containers import Requester
+from service.obj.exceptions import CreationError, InvalidValue, InvalidTypeValue, ReceivedEmptyDataFrame, InvalidModuleName
+from service.obj.request_record_from_sqlyte import RequestRecordFromSQLyte
 
 __all__ = [
     # Abstractclass
@@ -41,13 +41,7 @@ __all__ = [
 
 
 # if __name__ == "__main__":
-#
 #     container = Requester()
-#     requester = container.requester()
-#     print(requester)
-#     reader = container.reader()
-#     print(reader)
-#
 #
 #     settings = {'path': ':memory:', 'tablename': ':memory:', 'requester_type': 'sqlite', 'reader_type': 'list'}
 #     container.config.from_dict(settings)
